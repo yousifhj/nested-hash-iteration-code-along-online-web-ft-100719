c5416095496118670a1475ffb8b200676fdc81f5
Huda Yousif <hjdoski@gmail.com>
2019-10-13 18:25:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,12 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
-  contacts.each do |person, contact_details_hash|
- if person == "Freddy Mercury"
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
+end
 end
 
